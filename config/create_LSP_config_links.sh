1dcb64f78cad502a87da3fc19a8336f4067bd70d
Aaron Asencio <asencio.aaron@gmail.com>
2021-10-21 11:23:59 -0700

    script to link config files from animatronic to LSP

---
+++ config/create_LSP_config_links.sh
@@ -0,0 +1,3 @@
+# create config links from animatronic to LSP
+ln -s /home/pi/workspace/animatronic/config/overrides-mic.cfg /home/pi/workspace/lightshowpi/config/overrides-mic.cfg
+ln -s /home/pi/workspace/animatronic/config/overrides.cfg /home/pi/workspace/lightshowpi/config/overrides.cfg 
